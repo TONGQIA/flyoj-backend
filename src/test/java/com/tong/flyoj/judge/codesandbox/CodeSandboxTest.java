b1e7e1642a841e9e8fc05afc33070c7357b2bba1
TONGQIA <1227894812@qq.com>
2024-02-21 23:15:47 +0800

    Feat:调通远程代码沙箱

--- src/test/java/com/tong/flyoj/judge/codesandbox/CodeSandboxTest.java
+++ src/test/java/com/tong/flyoj/judge/codesandbox/CodeSandboxTest.java
@@ -20,7 +20,14 @@ class CodeSandboxTest {
 
     @Test
     void executeCode() {
-        String code = "int main(){}";
+        String code = "public class Main {\n" +
+                "\n" +
+                "    public static void main(String[] args) {\n" +
+                "        int a = Integer.parseInt(args[0]);\n" +
+                "        int b = Integer.parseInt(args[1]);\n" +
+                "        System.out.println(\"结果：\"+ (a + b));\n" +
+                "    }\n" +
+                "}";
         String language = QuestionSubmitLanguageEnum.JAVA.getValue();
         List<String> list = Arrays.asList("1 2", "2 3");
 
